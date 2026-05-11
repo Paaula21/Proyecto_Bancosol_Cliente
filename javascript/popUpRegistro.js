65e91533bd861d5422c9b58ae25f43f7f8828244
ainhoaagarciiaa <ainhoitagar05@gmail.com>
2026-05-11 12:24:09 +0200

    Pop Up Ready

--- javascript/popUpRegistro.js
+++ javascript/popUpRegistro.js
@@ -2,10 +2,23 @@ var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
     overlay = document.getElementById('overlay'),
     popup = document.getElementById('popup'),
     btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+    overlay2 = document.getElementById('overlay2'),
+    popup2 = document.getElementById('popup2'),
 
-btnAbrirPopup.addEventListener('click', function () {
-    overlay.classList.add('active');
-    popup.classList.add('active');
+btnAbrirPopup.addEventListener('click', function (e) {
+
+    e.preventDefault();
+
+    //Checkea que no se abra el pop up sin que los datos estén incompletos
+    const formulario = document.getElementById("form-voluntario");
+
+    if(formulario.checkValidity()) {
+        overlay.classList.add('active');
+        popup.classList.add('active');
+    } else {
+        overlay2.classList.add('active');
+        popup2.classList.add('active');
+    }
 });
 
 btnCerrarPopup.addEventListener('click', function (e) {
@@ -13,3 +26,5 @@ btnCerrarPopup.addEventListener('click', function (e) {
     overlay.classList.remove('active');
     popup.classList.remove('active');
 });
+
+
