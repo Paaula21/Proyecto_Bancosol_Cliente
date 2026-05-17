43ff9852057f596c818d3c958cb20d2bd7c718a8
Paula <paulafj2108@gmail.com>
2026-05-17 21:16:52 +0200

    Created general Header and implemented on Colaboradores

---
+++ javascript/Header.js
@@ -0,0 +1,27 @@
+document.addEventListener('DOMContentLoaded', () => {
+    
+    const rutaActual = window.location.pathname.split('/').pop() || "Administrador.html"; 
+    
+    // Obtener el header
+    const buscadorHeader = setInterval(() => {
+        const headerTitle = document.getElementById('dynamic-header-title');
+        const headerSubtitle = document.getElementById('dynamic-header-subtitle');
+        const enlacesMenu = document.querySelectorAll('.sidebar ul li a');
+
+        if (headerTitle && headerSubtitle && enlacesMenu.length > 0) {
+            clearInterval(buscadorHeader); // Apagamos el buscador
+
+            // Obtenemos los valores del header
+            enlacesMenu.forEach(enlace => {
+                const href = enlace.getAttribute('href');
+                if (href && href.includes(rutaActual)) {
+                    const titulo = enlace.getAttribute('data-titulo');
+                    const subtitulo = enlace.getAttribute('data-subtitulo');
+
+                    if (titulo) headerTitle.textContent = titulo;
+                    if (subtitulo) headerSubtitle.textContent = subtitulo;
+                }
+            });
+        }
+    }, 50);
+});
